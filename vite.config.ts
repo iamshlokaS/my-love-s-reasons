d8f03665e763f3207c097ebc7e200b81a3ac6204
Shloka Srivastava <shloka0209@gmail.com>
2025-11-19 20:34:09 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ⬅️ ADD THIS LINE
+  base: '/my-love-s-reasons/', 
+  // ⬅️ ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
